3710e04acff343d45e7b25bddf7c41020299a97a
Dan Mayer <dan.mayer@stitchfix.com>
2019-11-29 10:51:17 -0700

    init

---
+++ spec/views/posts/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "posts/index", type: :view do
+  before(:each) do
+    assign(:posts, [
+      Post.create!(
+        :title => "Title",
+        :body => "MyText",
+        :score => 2
+      ),
+      Post.create!(
+        :title => "Title",
+        :body => "MyText",
+        :score => 2
+      )
+    ])
+  end
+
+  it "renders a list of posts" do
+    render
+    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+  end
+end
---
+++ spec/views/posts/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "posts/show", type: :view do
+  before(:each) do
+    @post = assign(:post, Post.create!(
+      :title => "Title",
+      :body => "MyText",
+      :score => 2
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/2/)
+  end
+end
