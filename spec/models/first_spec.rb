df240e17c177d6f9affeea0b65cea75c1d91048a
Dan Mayer <danmayer@gmail.com>
2020-01-06 14:27:44 -0700

    cleaned up for flaky branch

--- spec/models/first_spec.rb
+++ spec/models/first_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
+# Classification: Shared State, Order Dependency
+# Success Rate: 99%
+# Suite Required: true
 RSpec.describe Post, type: :model do
 
   let(:post) { Post.create!(title: 'first', body: 'post', score: 1) }
 
-  # This spec passes when run alone, or if it happens to be the very first spec of the suite
   describe "post created" do
     it "can be updated" do
       post.update!(title: 'updated')
-      expect(post.id).to eq 1
+      expect(post.id).to_not eq 1
     end
   end
 end
